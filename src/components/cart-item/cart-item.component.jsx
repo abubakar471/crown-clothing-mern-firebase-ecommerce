7b1d78282340bf7429fe635aa30f2b8e66345918
abubakar471 <abdurjoy2001@gmail.com>
2023-07-27 11:55:59 -0700

    added in addToCart feature

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,17 @@
+import './cart-item.styles.scss'
+
+const CartItem = ({ cartItem }) => {
+    const { name, imageUrl, price, quantity } = cartItem;
+
+    return (
+        <div className='cart-item-container'>
+            <img src={imageUrl} alt={`${name}`} loading='lazy' />
+            <div className='item-details'>
+                <span className='name'>{name}</span>
+                <span className='price'>{price} x {quantity}</span>
+            </div>
+        </div>
+    )
+}
+
+export default CartItem
