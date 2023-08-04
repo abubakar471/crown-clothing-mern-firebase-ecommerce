158a563d85ee42e339fca1117edcf2cc5630928e
abubakar471 <abdurjoy2001@gmail.com>
2023-08-04 21:09:58 +0600

    reducers added

--- src/components/contexts/user.context.jsx
+++ src/components/contexts/user.context.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import { createUserDocumentFromAuth, onAuthStateChangedListener } from "../../utils/firebase/firebase.utils";
+import { createAction } from "../../utils/reducer/reducer.utils";
 
 // as the actual value you want to access
 export const UserContext = createContext({
@@ -7,8 +8,41 @@ export const UserContext = createContext({
     setCurrentUser: () => null
 });
 
+const USER_ACTION_TYPE = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+    console.log('dispatched')
+    switch (type) {
+        case USER_ACTION_TYPE.SET_CURRENT_USER:
+            return {
+                currentUser: payload
+            }
+
+        default:
+            throw new Error(`unhandlerd tyoe ${type} in userReducer`)
+    }
+
+
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    // const [currentUser, setCurrentUser] = useState(null);
+    const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+    const { currentUser } = state;
+
+    const setCurrentUser = (user) => {
+        dispatch(createAction(USER_ACTION_TYPE.SET_CURRENT_USER, user));
+    }
+
     const value = {
         currentUser, setCurrentUser
     }
